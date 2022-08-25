752bc4de5102222833360cff173091928c19b356
Arslan <rslnkrmt2552@gmail.com>
2022-08-26 00:16:42 +0500

    0.0.1

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,70 @@
-import test from 'ava'
+import test from "ava";
 
-import { sum } from '../index.js'
+import { sum, Queue } from "../index.js";
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
-})
+test.beforeEach("generate queue object", (t) => {
+  t.context.q = new Queue();
+});
+
+test("sum from native", (t) => {
+  t.is(sum(1, 2), 3);
+});
+
+test("queue is constructed with 0 len", (t) => {
+  /**
+   * @type {Queue}
+   */
+  const q = t.context.q;
+
+  t.is(q.len(), 0);
+  t.true(q.isEmpty());
+});
+
+test("queue length should increase by one on push back", (t) => {
+  /**
+   * @type {Queue}
+   */
+  const q = t.context.q;
+
+  t.true(q.isEmpty());
+
+  q.push(2);
+
+  t.is(q.len(), 1);
+});
+
+test("pop on empty returns none", (t) => {
+  /**
+   * @type {Queue}
+   */
+  const q = t.context.q;
+
+  t.is(q.pop(), null);
+});
+
+test("push and pop in succession return the same val", (t) => {
+  /**
+   * @type {Queue}
+   */
+  const q = t.context.q;
+  const val = 2;
+
+  q.push(val);
+
+  t.is(q.pop(), val);
+});
+
+test("queue is fifo", (t) => {
+  /**
+   * @type {Queue}
+   */
+  const q = t.context.q;
+  const val1 = 13;
+  const val2 = 42;
+
+  q.push(val1);
+  q.push(val2);
+
+  t.is(q.pop(), val1);
+  t.is(q.pop(), val2);
+});
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,55 @@
 #![deny(clippy::all)]
 
+use std::collections::VecDeque;
+
 #[macro_use]
 extern crate napi_derive;
 
 #[napi]
 pub fn sum(a: i32, b: i32) -> i32 {
   a + b
+}
+
+#[napi(js_name = "Queue")]
+#[derive(Debug, Default)]
+pub struct Queue {
+  q: VecDeque<i64>,
+}
+
+#[napi]
+impl Queue {
+  #[napi(constructor)]
+  pub fn new() -> Self {
+    Self { q: VecDeque::new() }
+  }
+
+  #[napi]
+  pub fn push(&mut self, val: i64) {
+    self.q.push_back(val);
+  }
+
+  #[napi]
+  pub fn pop(&mut self) -> Option<i64> {
+    self.q.pop_front()
+  }
+
+  #[napi]
+  pub fn peek(&mut self) -> Option<i64> {
+    let q_len = self.q.len();
+
+    match q_len {
+      0 => None,
+      _ => Some(self.q[q_len - 1]),
+    }
+  }
+
+  #[napi]
+  pub fn len(&self) -> i64 {
+    self.q.len() as i64
+  }
+
+  #[napi]
+  pub fn is_empty(&self) -> bool {
+    self.q.is_empty()
+  }
 }
